ab5de6627fe64988f87a69854ea18d852563de04
Tom Hoag <tomhoag@users.noreply.github.com>
2026-03-11 14:07:41 -0400

    Add keywords to Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "Clusterables",
+    keywords: ["cluster", "clustering", "MapKit"],
     platforms: [
         .iOS(.v17),
         .macOS(.v14),
